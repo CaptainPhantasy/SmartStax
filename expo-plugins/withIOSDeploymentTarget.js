343aa8e420965fb6454416800d680b260b259f74
CaptainPhantasy <DouglasTalley1977@gmail.com>
2025-09-12 01:40:26 -0400

    Remove plugin approach, rely on Xcode 16 image for iOS 18 compatibility

--- expo-plugins/withIOSDeploymentTarget.js
+++
@@ -1,8 +0,0 @@
-const { withInfoPlist } = require('expo/config-plugins');
-
-module.exports = function withIOSDeploymentTarget(config) {
-  return withInfoPlist(config, (config) => {
-    config.modResults.MinimumOSVersion = '13.4';
-    return config;
-  });
-};
